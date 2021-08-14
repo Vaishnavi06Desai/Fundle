b26cdb6f2d84017f412febf57f838283da25184a
Vaishnavi <vaishnavisdesai@gmail.com>
2021-08-14 20:35:11 +0530

    ...

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,7 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { NavdrawerComponent } from './navdrawer/navdrawer.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ProjectsComponent } from './projects/projects.component';
 
 @NgModule({
   declarations: [
@@ -20,7 +21,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     SigninComponent,
     HomeComponent,
     NavbarComponent,
-    NavdrawerComponent
+    NavdrawerComponent,
+    ProjectsComponent
   ],
   imports: [
     BrowserModule,
