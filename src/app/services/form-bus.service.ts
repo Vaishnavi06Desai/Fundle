8ceb842722cbc7f087628636287db3a64ea06ae4
Vaishnavi <vaishnavisdesai@gmail.com>
2021-08-16 01:35:27 +0530

    ..

--- src/app/services/form-bus.service.ts
+++ src/app/services/form-bus.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/firestore';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FormBusService {
 
-  constructor() { }
+  constructor(private db: AngularFirestore) { }
 
-  onSubmit(formno: number, formvalue: any){
-    if(formno == 1){
+  onSubmit(formno: number, uid: string, formvalue: any){
+    if(formno == 0){
       console.log(formvalue);
+    }
+
+    else if(formno == 1){
+      this.db.collection("Workspaces").doc(uid).collection("Projects").add(formvalue);
     }
   }
 }
