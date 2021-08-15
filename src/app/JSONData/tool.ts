7b5707f9574ed5e217314aa262a0e832a9da612a
unknown <jhanvisdesai@gmail.com>
2021-08-15 15:12:28 +0530

    add icons in workspaces

---
+++ src/app/JSONData/tool.ts
@@ -0,0 +1,8 @@
+export let tools = [
+    {
+        "icon":"../../assets/images/trending.png",
+        "link":"",
+        "alttext":"",
+        "tooltip":""
+    }
+]
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,22 +1,27 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninComponent } from './signin/signin.component';
-  import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './home/home.component';
 import { ProjectsComponent } from './projects/projects.component';
-  
+import { WorkspacesComponent } from './workspaces/workspaces.component';
+
 const routes: Routes = [
-    {
-        path: 'home',
-        component: HomeComponent
-    },
-   {
-        path: 'signin',
-        component: SigninComponent
-    },
-    {
-      path: 'projects',
-      component: ProjectsComponent
+  {
+    path: 'home',
+    component: HomeComponent
   },
+  {
+    path: 'signin',
+    component: SigninComponent
+  },
+  {
+    path: 'Workspaces',
+    component: WorkspacesComponent
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent
+  }
 ];
 
 @NgModule({
