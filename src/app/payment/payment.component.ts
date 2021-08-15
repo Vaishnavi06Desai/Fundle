1122c77774e8e1343b5d2ce2af55d487d3229be9
anika-001 <dream.sweeter.30.09@gmail.com>
2021-08-15 15:18:25 +0530

    commit

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,15 +1,46 @@
 import { Component, OnInit } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
   styleUrls: ['./payment.component.scss']
 })
 export class PaymentComponent implements OnInit {
-
-  constructor() { }
+  //json: any;
+  json : {
+    "amount": 1000,
+    "currency": "INR",
+    "reference_id": "Fundle101",
+    "description": "Payment Test Fundle",
+    "customer": {
+      "name": "Anika Tibrewala",
+      "contact": "+91708252413",
+      "email": "anikatibrewala@gmail.com"
+    },
+    "notify": {
+      "sms": false,
+      "email": true
+    },
+    "reminder_enable": true,
+  }
+  constructor(private httpClient: HttpClient) { }
 
   ngOnInit(): void {
-  }
 
+  }
+  payment() {
+
+    this.httpClient.post<any>('https://api.razorpay.com/v1/payment_links/',this.json, {
+      headers: { 'Basic': 'cnpwX3Rlc3RfSVYzeUtxbnNrRk92VWc6QmFPbUtlSTF3dFF2bjlKRHRSZ3RGSnZZ' }
+    }).subscribe(
+      (res) => {
+        console.log(res);
+       
+      },
+      (err) => {
+        console.log(err);
+    
+      }
+    );
+  }
 }
