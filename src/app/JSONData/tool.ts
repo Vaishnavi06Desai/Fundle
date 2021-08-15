3b01e577ad985a0bd72a75607de2cee8a6a33126
unknown <jhanvisdesai@gmail.com>
2021-08-15 18:48:08 +0530

    get changes

--- src/app/JSONData/tool.ts
+++ src/app/JSONData/tool.ts
@@ -21,3 +21,26 @@ export let tools = [
         // "func":"(click)='hey()'"
     }
 ]
+export let tools2 = [
+    {
+        "icon":"../../assets/images/dashboard.png",
+        "link":"",
+        "alttext":"Dashboard",
+        "tooltip":"Dashboard",
+        // "func":"(click)='hey()'"
+    },
+    {
+        "icon":"../../assets/images/requested.png",
+        "link":"",
+        "alttext":"Requested",
+        "tooltip":"Requested",
+        // "func":"(click)='hey()'"
+    },
+    {
+        "icon":"../../assets/images/payout.png",
+        "link":"",
+        "alttext":"Payout",
+        "tooltip":"Payout",
+        // "func":"(click)='hey()'"
+    }
+]
--- src/app/invoices/invoices.component.ts
+++ src/app/invoices/invoices.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { tools2 } from '../JSONData/tool';
 
 @Component({
   selector: 'app-invoices',
@@ -7,9 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InvoicesComponent implements OnInit {
 
+  tools:any;
+  invpopup:boolean;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.tools=tools2;
+    this.invpopup=false;
+  }
+  addinvoices() {
+    this.invpopup = true;
+    console.log("Project added");
+  }
+  Submit(){
+    this.invpopup = false;
   }
 
 }
